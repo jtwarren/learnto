06629ce4460ad59f6be7b4d7d45d2a8d22add8b7
dhruvp <dhruv@dhcp-18-111-15-76.dyn.mit.edu>
2013-10-19 19:53:23 -0400

    working on adding new fields

--- app/assets/javascripts/registration.js.coffee
+++ app/assets/javascripts/registration.js.coffee
@@ -4,5 +4,19 @@ ready = ->
 		link.prev("input[type=hidden]").value = "1"
 		link.parents(".fields").hide()
 		return false
+
+	$('.add_field').click ->
+		link=$('.add_field')
+		association=$(this).data["association"]
+		content=$(this).data["fields"]
+		add_fields(link, association, content)
+
 $(document).ready(ready)
 $(document).on('page:load', ready)
+
+add_fields (link, association, content) ->
+	new_id = new Date().getTime()
+	regexp = new RegExp("new_"+association, "g")
+	$(link).prev().insert({
+		before: content.replace(regexp,new_id)
+	})
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 	def link_to_remove_fields(name, f)
 		f.hidden_field(:_destroy)+link_to(name,'#', :class => "remove_field")
 	end
+
+	def link_to_add_fields(name, f, association)
+	new_object = f.object.class.reflect_on_association(association).klass.new
+	fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+	  render(association.to_s.singularize + "_fields", :f => builder)
+	end
+	content_tag(:a, name, "href"=>"#", "data-association"=> '#{association}', "data-fields"=>'#{escape_javascript(fields)}', :class => "add_field")
+	end
 end
