4e789c9e30e9cc7a5997c3d3a82f3f760b46a682
dhruvp <dhruv@Dhruvs-MacBook.local>
2013-11-01 18:11:32 -0400

    fixed facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 595637847160806, 'fbf5263cc6e5db1d75eb21214844d135'
+  provider :facebook, 513963515360226, '33068b37725db803e4cffa2ee1e26db1'
 end
