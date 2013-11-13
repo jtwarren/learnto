b220cff9c94c712e6accae6afc9f5310b55939a4
dhruvp <dhruvp@mit.edu>
2013-11-13 09:32:42 -0500

    fixing lesson signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,12 +58,13 @@ class UsersController < ApplicationController
     if params[:skill_id]
       skill = Skill.find(params[:skill_id])
       message = Message.create(body: params[:request], sender: current_user.id, receiver:skill.user.id)
-      lesson = skill.lessons.new(:learning_request=>params[:request])
-      # c=Conversation.create()
-      # c.messages << message
-      # c.receipts.create(user_id: current_user.id, read: true)
-      # c.receipts.create(user_id: skill.teacher.id, read: false)
-      # lesson.conversation = c
+      #lesson = skill.lessons.new(:learning_request=>params[:request])
+      lesson= skill.lessons.new
+      c=Conversation.create()
+      c.messages << message
+      c.receipts.create(user_id: current_user.id, read: true)
+      c.receipts.create(user_id: skill.teacher.id, read: false)
+      lesson.conversation = c
     else
       lesson = Lesson.new(:learning_request=>params[:request])
     end
