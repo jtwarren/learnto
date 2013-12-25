47534898f2ca4c9bc1d01376e49f4b408627a8e5
Jeffrey Warren <jtwarren@mit.edu>
2013-12-24 23:22:20 -0500

    removed clustering

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -23,66 +23,7 @@ class Skill < ActiveRecord::Base
   end
 
   def similar_skills
-    skills = Skill.where("approved=? AND hidden=?", true, false)
-
-    @words = skills.map do |skill| 
-      skill.description.split(' ')
-    end.flatten.uniq
-
-    @vectors = skills.map do |skill|
-      @words.map do |w| 
-        skill.description.include?(w) ? 1 : 0 
-      end
-    end
-
-    number_centers = Skill.count / 5
-
-    @cluster_centers = @vectors.slice(0, number_centers)
-
-    15.times do
-      @clusters = Array.new(number_centers){[]}
-     
-      @vectors.each do |vector|
-        min_distance, min_point = 99999, 0
-     
-        @cluster_centers.each.with_index do |center, i|
-          if euclidean_distance(center, vector) < min_distance
-            min_distance = euclidean_distance(center, vector)
-            min_point = i
-          end
-        end
-     
-        @clusters[min_point] << vector
-      end
-     
-      @cluster_centers = @clusters.map do |vectors|
-        average(vectors)
-      end
-    end
-
-    c = nil
-    @clusters.each do |cluster|
-      cluster.each do |vector|
-        if self == skills[@vectors.index(vector)]
-          c = cluster
-        end
-      end
-    end
-
-
-
-    if c != nil
-      ret = []
-      c.each do |vector|
-        if self != skills[@vectors.index(vector)]
-          ret << skills[@vectors.index(vector)]
-        end
-      end
-      return ret.first(3)
-    end
-
-    return Skill.order("RANDOM()").first(3)
-
+    return Skill.where("approved=? AND hidden=?", true, false).order("RANDOM()").first(3)
   end
 
 
