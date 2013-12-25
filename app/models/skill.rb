96a404d26c32888b38dc3658e960d9f4880bf11a
Jeffrey Warren <jtwarren@mit.edu>
2013-12-24 23:01:37 -0500

    added similar skills using clustering

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,4 @@
+
 class Skill < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
@@ -22,8 +23,87 @@ class Skill < ActiveRecord::Base
   end
 
   def similar_skills
+    skills = Skill.all
+
+    @words = skills.map do |skill| 
+      skill.description.split(' ')
+    end.flatten.uniq
+
+    @vectors = skills.map do |skill|
+      @words.map do |w| 
+        skill.description.include?(w) ? 1 : 0 
+      end
+    end
+
+    number_centers = 10
+
+    @cluster_centers = @vectors.slice(0, number_centers)
+
+    15.times do
+      @clusters = Array.new(number_centers){[]}
+     
+      @vectors.each do |vector|
+        min_distance, min_point = 99999, 0
+     
+        @cluster_centers.each.with_index do |center, i|
+          if euclidean_distance(center, vector) < min_distance
+            min_distance = euclidean_distance(center, vector)
+            min_point = i
+          end
+        end
+     
+        @clusters[min_point] << vector
+      end
+     
+      @cluster_centers = @clusters.map do |vectors|
+        average(vectors)
+      end
+    end
+
+
+    c = nil
+    @clusters.each do |cluster|
+      cluster.each do |vector|
+        if self == skills[@vectors.index(vector)]
+          c = cluster
+        end
+      end
+    end
+
+
+    if c != nil
+      ret = []
+      c.each do |vector|
+        ret << skills[@vectors.index(vector)]
+      end
+      return ret.first(3)
+    end
+
     return Skill.order("RANDOM()").first(3)
+
   end
+
+
+
+
+
+  private
+
+    def euclidean_distance(vector1, vector2)
+      sum = 0
+      vector1.zip(vector2).each do |v1, v2|
+        component = (v1 - v2)**2 
+        sum += component 
+      end 
+      Math.sqrt(sum) 
+    end
+
+    def average(vectors)
+      vectors[0].zip(*vectors.slice(1, vectors.length)).map {|array| array.reduce(:+) / array.size }
+    end
+
+
+
 
 end
 
