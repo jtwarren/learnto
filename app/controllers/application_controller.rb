118867aee4cebe424937e858a71350892e527d03
Jeffrey Warren <jtwarren@mit.edu>
2014-02-13 21:10:15 -0500

    network stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,23 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  
   force_ssl if: :ssl_configured?
 
+  # before_filter :require_network
+
+
   def ssl_configured?
-    !Rails.env.development?
+    # !Rails.env.development?
+    false
   end
+
+  def require_network
+    if not current_network
+      return redirect_to root_url
+    end
+  end
+
+
 
   private
 
@@ -15,5 +25,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+  def current_network
+    @current_network ||= Network.find_by(subdomain: request.subdomain)
+  end
+
+  helper_method :current_user, :current_network
 end
